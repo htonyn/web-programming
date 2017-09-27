a77d2c2fa1d80e39981d43b5c22edf609b7d1802
htonyn-d <htonynguyen@gmail.com>
2017-09-27 01:04:46 -0400

    HW4 - Fixed styling function

--- assignments/homework/homework4/assignment4.php
+++ assignments/homework/homework4/assignment4.php
@@ -88,71 +88,71 @@
         $fontFamily = $_POST['select_font_family'];
         $fontSize = $_POST['fontSize'];
         $content = $_POST['content'];
-    }
 
-    switch ($textColor) {
-        case 0:
-            $textColor = "black";
-            break;
-        case 1:
-            $textColor = "red";
-            break;
-        case 2:
-            $textColor = "green";
-            break;
-        case 3:
-            $textColor = "blue";
-            break;
-        default:
-            $textColor = "white";
-            break;
-    }
-
-    switch ($fontFamily) {
-        case 0:
-            $fontFamily = "\"Times New Roman\", Times, serif";
-            break;
-        case 1:
-            $fontFamily = "Georgia, serif";
-            break;
-        case 2:
-            $fontFamily = "Impact, Charcoal, sans-serif";
-            break;
-        case 3:
-            $fontFamily = "Arial, Helvetica, sans-serif";
-            break;
-        default:
-            $fontFamily = "Tahoma, Geneva, sans-serif";
-            break;
-    }
-
-    echo "<style>body { color: ".$textColor."; font-family: ".$fontFamily."; font-size: ".$fontSize."; } </style>";
-
-    echo "<div id=\"content\">";
-    foreach ($_POST['fontStyle'] as $style) {
-        if ($style == 0) {
-            echo "<b>";
-        } else if ($style == 1) {
-            echo "<u>";
-        } else if ($style == 2) {
-            echo "<i>";
+        switch ($textColor) {
+            case 0:
+                $textColor = "black";
+                break;
+            case 1:
+                $textColor = "red";
+                break;
+            case 2:
+                $textColor = "green";
+                break;
+            case 3:
+                $textColor = "blue";
+                break;
+            default:
+                $textColor = "white";
+                break;
         }
-    }
-    if (is_null($content)) {
-        echo "Output will be printed here...";
-    } else {
-        echo $content;
-    }
-    foreach ($_POST['fontStyle'] as $style) {
-        if ($style == 0) {
-            echo "</b>";
-        } else if ($style == 1) {
-            echo "</u>";
-        } else if ($style == 2) {
-            echo "</i>";
+    
+        switch ($fontFamily) {
+            case 0:
+                $fontFamily = "\"Times New Roman\", Times, serif";
+                break;
+            case 1:
+                $fontFamily = "Georgia, serif";
+                break;
+            case 2:
+                $fontFamily = "Impact, Charcoal, sans-serif";
+                break;
+            case 3:
+                $fontFamily = "Arial, Helvetica, sans-serif";
+                break;
+            default:
+                $fontFamily = "Tahoma, Geneva, sans-serif";
+                break;
         }
+     
+        echo "<div id=\"content\" style=\"color: ".$textColor."; font-family: ".$fontFamily."; font-size: ".$fontSize."px\">";
+        foreach ($_POST['fontStyle'] as $style) {
+            if ($style == 0) {
+                echo "<b>";
+            } else if ($style == 1) {
+                echo "<u>";
+            } else if ($style == 2) {
+                echo "<i>";
+            }
+        }
+        if (is_null($content)) {
+            echo "Output will be printed here...";
+        } else {
+            echo $content;
+        }
+        foreach ($_POST['fontStyle'] as $style) {
+            if ($style == 0) {
+                echo "</b>";
+            } else if ($style == 1) {
+                echo "</u>";
+            } else if ($style == 2) {
+                echo "</i>";
+            }
+        }
+        echo "</div>";
     }
-    echo "</div>";
+
+    
     ?>
     <hr>
     <a href="assignment4.txt">PHP Source Code - Assignment 4 Part 1</a>
