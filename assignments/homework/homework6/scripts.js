f9418e3e11743bf54556ec421542193c7206324d
Hoan Nguyen <htonynguyen@gmail.com>
2017-10-19 01:31:43 -0400

    Homework 6 - Part C

--- assignments/homework/homework6/scripts.js
+++
@@ -1,8 +0,0 @@
-function tabSwap(tabName, contentSet) {
-    $('#'+contentSet).children().hide();
-    $('#'+tabName).show();
-}
-
-function test() {
-    document.getElementById('part-c').innerHTML = "Potato";
-}
--- assignments/homework/homework6/scripts/scripts.js
+++ assignments/homework/homework6/scripts/scripts.js
@@ -4,6 +4,7 @@ globalTimer = 150;
 globalValue = -1;
 globalGuesses = 10;
 gameToggle = 0;
+matching = 0;
 
 function tabSwap(tabName, contentSet) {
     $('#'+contentSet).children().hide();
@@ -41,8 +42,11 @@ function guessingGame() {
     } else {
         if (inputValue == globalValue) {
             document.getElementById('b-print').innerHTML = "You did it!";
+            document.getElementById('b-output').innerHTML += "Guess #"+(10-globalGuesses)+": "+inputValue+" - Correct<br>";
             clearInterval(intervalControl);
             gameToggle = 0;
+            globalTimer = 150;
+            initializeGuessing();
         } else if (inputValue > globalValue) {
             document.getElementById('b-print').innerHTML = "Too high!";
             globalGuesses--;
@@ -66,7 +70,7 @@ function initializeGuessing() {
             } else {
                 document.getElementById('time-id').innerHTML = globalTimer--;
             }            
-        }, 50);
+        }, 1000);
         globalValue = Math.floor(Math.random() * 100);
         globalGuesses = 10;
         // document.getElementById('b-print-2').innerHTML = "Value: "+globalValue;
@@ -81,6 +85,45 @@ function initializeGuessing() {
     guessingGame();
 }
 
-function matchCards() {
-    
+function potato(counterCard) {
+    var card = [{id: 1, image: 1},
+                {id: 2, image: 2},
+                {id: 3, image: 2},
+                {id: 4, image: 2},
+                {id: 5, image: 2},
+                {id: 6, image: 2},
+                {id: 7, image: 2},
+                {id: 8, image: 2},
+                {id: 9, image: 2},
+                {id: 10, image: 2},
+                {id: 11, image: 2},
+                {id: 12, image: 2},
+    ];
+    var arrayCards = [card[0], card[1], 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    console.log(arrayCards[counterCard].id);
+    document.getElementById('test-value').innerHTML = "You selected "+arrayCards[counterCard].id;
+}
+
+function matchStart() {
+    duration = 1;
+    matchesCount = 0;
+    matchCards = document.getElementById('selectPairs').value;
+    counter = 0;
+    if (true) {
+        matchTimer = 0;
+        matchCounter = setInterval(function() {
+            matchTimer++;
+        }, 1000);
+        matching = 1;
+        matchDisplay = '<table>';
+        for (i = 0; i < 4; i++) {
+            matchDisplay += '<tr>';
+            for(j = 0; j<matchCards/2; j++) {
+                matchDisplay += '<td id='+i+'-'+j+' height="100px" width="100px" onclick="potato('+(counter++)+')">'+counter+'</td>';
+            }
+            matchDisplay += '</tr>';
+        }
+        matchDisplay += '</table>';
+    }
+    document.getElementById('match-table').innerHTML = matchDisplay;
 }
